6fe6ec163c5af910c173ea19926c6edb2b2ecc25
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-05-02 20:32:49 +0800

    generated sample balances obj with checksumed addresses

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const secp = require("@noble/secp256k1");
+const { ethers } = require("ethers");
 const port = 3042;
 
 // localhost can have cross origin errors
@@ -8,10 +10,52 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
+
+/*--------------------------  THE FOLLOWING BLOCK WILL LOG A RANDOMLY GENERATED BALANCES OBJ TO THE CONSOLE      --------------------------------*/
+
+/*
+const SAMPLE_INITIAL_BALANCE = 100;
+const NUM_ADDRESSES = 3;
+
+
+
+function getSampleBalance() {
+  const addresses = getRandomAddresses();
+
+  const sampleBalance = {};
+  addresses.map(pubKey => sampleBalance[pubKey] = SAMPLE_INITIAL_BALANCE);
+
+  return sampleBalance;
+}
+
+
+function getRandomAddresses() {
+  const publicKeyArray = [];
+  for(let i = 0; i < NUM_ADDRESSES; i++) {
+    publicKeyArray.push(getRandomAddress());
+  }
+  return publicKeyArray;
+
+}
+
+
+function getRandomAddress() {
+  const privateKey = secp.utils.randomPrivateKey();
+  const publicKey = secp.getPublicKey(privateKey);
+  const publicKeyHex = `0x${secp.utils.bytesToHex(publicKey).slice(0,40)}`;
+  const address = ethers.utils.getAddress(publicKeyHex);
+  return address;
+}
+
+console.log(getSampleBalance());
+*/
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  '0x040D9B84dA124C1Cf7E5a3056F9453B429c37849': 100,
+  '0x04081dCc6BBE0Ee7ebF396e3fEEb05c4153aEF45': 100,
+  '0x04012bC51ae7ba5890B8754B2F0d899984581FBd': 100
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -27,6 +71,16 @@ app.post('/send', (req, res) => {
   res.send({ balance: balances[sender] });
 });
 
+
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+
+
 });
+
+
+
+
+
